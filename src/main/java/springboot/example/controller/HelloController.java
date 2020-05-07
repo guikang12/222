9d81e74756a45cdf16f3f8d3d6c8103e38c65112
guikang <1021854925@qq.com>
2020-05-07 20:54:55 +0800

    测试

---
+++ src/main/java/springboot/example/controller/HelloController.java
@@ -0,0 +1,13 @@
+package springboot.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    String home(){
+        return "hello";
+    }
+
+}
